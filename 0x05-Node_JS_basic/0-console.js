9f6c1926e242c9058ccee22c7e27428dc19addb5
KIHALAya <kihalaya00@gmail.com>
2024-07-25 10:04:07 +0000

    Task 1

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+// 0-console.js
+
+// Define the displayMessage function
+function displayMessage(message) {
+  console.log(message);
+}
+
+// Export the function to make it available for importing in other files
+module.exports = displayMessage;
+
