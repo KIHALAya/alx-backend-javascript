df05be1ec4a6e010a13b9ed130b1aea853ad2e0f
KIHALAya <133595209+KIHALAya@users.noreply.github.com>
2024-11-26 23:37:31 +0100

    Delete 0x05-Node_JS_basic directory

--- 0x05-Node_JS_basic/0-console.js
+++
@@ -1,9 +0,0 @@
-// 0-console.js
-
-// Define the displayMessage function
-function displayMessage(message) {
-  console.log(message);
-}
-
-// Export the function to make it available for importing in other files
-module.exports = displayMessage;
